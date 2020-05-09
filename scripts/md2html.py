7d68fa839be168bd45ac4185eaabeb70d810b1be
Henry Gebhardt <hsggebhardt@gmail.com>
2020-05-09 11:14:10 -0700

    automatically generate index.html

--- scripts/md2html.py
+++ scripts/md2html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-from markdown2 import markdown_path
+from markdown2 import markdown, markdown_path
 
 top = """<!DOCTYPE html>
 <html>
@@ -16,7 +16,6 @@ mdfiles = [
         "../2017/blog-20170604.md",
         "../2017/blog-20170605.md",
         "../2017/blog-20170614.md",
-        "../2017/thingsnobodycaresaboutbutme.md",
         "../2018/blog-20180101.md",
         "../2018/blog-20180118.md",
         "../2018/blog-20180326.md",
@@ -25,6 +24,7 @@ mdfiles = [
         "../2018/blog-20181121.md",
         "../2019/blog-20190324.md",
         "../2020/blog-20200426.md",
+        "../2017/thingsnobodycaresaboutbutme.md",
         ]
 
 
@@ -39,21 +39,45 @@ def writefile(path, contents):
         f.write(contents)
 
 
-head = readfile("snippets/head.html")
-footer = readfile("snippets/footer.html")
+def readfirstline(path):
+    with open(path, "r") as f:
+        line = f.readline()
+    return line
 
-def md2html(path):
-    body = "\n".join(["<body>", markdown_path(path), "</body>"])
-    html = "\n".join([top, head, body, footer, bottom])
-    return html
+
+def path_md2html(mdfile):
+    htmlfile = ".".join([os.path.splitext(mdfile)[0], "html"])
+    return htmlfile
 
 
 def main():
+    head = readfile("snippets/head.html")
+    footer = readfile("snippets/footer.html")
+
     # convert md files to html
     for mdfile in mdfiles:
-        htmlfile = ".".join([os.path.splitext(mdfile)[0], "html"])
+        htmlfile = path_md2html(mdfile)
         print("Converting", mdfile, "->", htmlfile, "...")
-        html = md2html(mdfile)
+        body = "\n".join(["<body>", markdown_path(mdfile), "</body>"])
+        html = "\n".join([top, head, body, footer, bottom])
         writefile(htmlfile, html)
+
+    # generate index.html
+    indexpre = markdown_path("snippets/index-pre.html")
+    toc = ""
+    for mdfile in mdfiles:
+        htmlfile = path_md2html(mdfile)
+        date = mdfile[13:-3]
+        title = date[:4] + "-" + date[4:6] + "-" + date[6:] + ": "
+        if len(title) != 12:
+            title = ""
+        title += readfirstline(mdfile)[2:-1]
+        print(title)
+        toc += "\n  - [" + title + "](" + htmlfile + ")"
+    # other entries
+    toc += "\n  - [Shue's blog](../2018/shuesblog/)"
+    toc = markdown(toc)
+    html = "\n".join([top, head, "<body>", indexpre, toc, "</body>", footer, bottom])
+    writefile("../2020/index.html", html)
 
 main()
