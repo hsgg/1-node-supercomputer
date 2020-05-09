5d1a33c62578d6d07e19d46c7fba1ce83212f3c4
Henry Gebhardt <hsggebhardt@gmail.com>
2020-05-09 10:47:14 -0700

    add footer

--- scripts/md2html.py
+++ scripts/md2html.py
@@ -11,6 +11,22 @@ bottom = """
 </html>
 """
 
+mdfiles = [
+        "../2017/blog-20170520.md",
+        "../2017/blog-20170604.md",
+        "../2017/blog-20170605.md",
+        "../2017/blog-20170614.md",
+        "../2017/thingsnobodycaresaboutbutme.md",
+        "../2018/blog-20180101.md",
+        "../2018/blog-20180118.md",
+        "../2018/blog-20180326.md",
+        "../2018/blog-20180516.md",
+        "../2018/blog-20180601.md",
+        "../2018/blog-20181121.md",
+        "../2019/blog-20190324.md",
+        "../2020/blog-20200426.md",
+        ]
+
 
 def readfile(path):
     with open(path, "r") as f:
@@ -23,35 +39,17 @@ def writefile(path, contents):
         f.write(contents)
 
 
+head = readfile("snippets/head.html")
+footer = readfile("snippets/footer.html")
+
 def md2html(path):
-    # head
-    head = readfile("snippets/head.html")
-
-    # body
     body = "\n".join(["<body>", markdown_path(path), "</body>"])
-
-    # full html
-    html = "\n".join([top, head, body, bottom])
+    html = "\n".join([top, head, body, footer, bottom])
     return html
 
 
 def main():
-    mdfiles = [
-            "../2017/blog-20170520.md",
-            "../2017/blog-20170604.md",
-            "../2017/blog-20170605.md",
-            "../2017/blog-20170614.md",
-            "../2017/thingsnobodycaresaboutbutme.md",
-            "../2018/blog-20180101.md",
-            "../2018/blog-20180118.md",
-            "../2018/blog-20180326.md",
-            "../2018/blog-20180516.md",
-            "../2018/blog-20180601.md",
-            "../2018/blog-20181121.md",
-            "../2019/blog-20190324.md",
-            "../2020/blog-20200426.md",
-            ]
-
+    # convert md files to html
     for mdfile in mdfiles:
         htmlfile = ".".join([os.path.splitext(mdfile)[0], "html"])
         print("Converting", mdfile, "->", htmlfile, "...")
