b5e39974c21c4ce0d2e2280fc5c1205acd9dd22c
Henry Gebhardt <hsggebhardt@gmail.com>
2020-06-21 14:43:14 -0700

    taxes, taxes, taxes

--- scripts/md2html.py
+++ scripts/md2html.py
@@ -28,6 +28,7 @@ mdfiles = [
         "../2020/blog-20200426.md",
         "../2020/blog-20200523.md",
         "../2020/blog-20200531.md",
+        "../2020/blog-20200621.md",
         "../2017/thingsnobodycaresaboutbutme.md",
         ]
 
